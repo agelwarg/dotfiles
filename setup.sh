36c342c6d1c68483b15127b882b209f685e3071f
Adam Gelwarg <adam@gelwarg.net>
2016-07-05 19:13:51 +0000

    Initial ssh setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 _dir="$(cd $(dirname $0); pwd)"
 
+# SSH setup
+mkdir -p ~/.ssh chmod 700 ~/.ssh
+for i in ${_dir}/ssh/*; do
+  [ -f ~/.ssh/$(basename $i) ] || cp $i ~/.ssh/$(basename $i)
+done
+
 for i in vim vimrc screenrc inputrc; do
   rm -rf ~/.$i && ln -s ${_dir}/$i ~/.$i
 done
